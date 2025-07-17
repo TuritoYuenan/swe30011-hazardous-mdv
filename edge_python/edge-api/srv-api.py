eed92df207e866e61197a4cae09b89aeb25049fd
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-07-17 16:58:24 +1000

    rename database file for python edge server

--- edge_python/edge-api/srv-api.py
+++ edge_python/edge-api/srv-api.py
@@ -10,7 +10,7 @@ import json
 
 # Linux: `/dev/tty{USB,ACM}#`, Windows: `COM#`
 ARDUINO_PORT = os.getenv("ARDUINO_PORT", 'COM8')
-DATABASE_FILE = os.getenv("DATABASE_FILE", 'database-edge/sqlite.db')
+DATABASE_FILE = os.getenv("DATABASE_FILE", 'sensor_data.db')
 DATABASE_TABLE = os.getenv("DATABASE_TABLE", 'readings')
 
 arduino = serial.Serial(ARDUINO_PORT, 9600, timeout=1)
